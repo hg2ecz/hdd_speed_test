36c574623b33e52f8f00f5ed69b56e724b2a9a58
Zsolt Krüpl <hg2ecz@ham.hu>
2024-02-22 14:19:52 +0100

    -t <thread> and -k

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,23 @@
 use clap::{Arg, ArgAction, Command};
 use memmap::{MmapMut, MmapOptions};
 use rand::prelude::*;
-use std::fs::File;
+use std::fs::{self, File};
 use std::io::Write;
+use std::thread;
 use std::time;
 
-const FNAME: &str = "testfile.dat";
+const DIRNAME: &str = "hdd_speed_test_tmpdir";
+const FILENAME: &str = "testfile.dat";
+const CHUNK_N_4K: u64 = 1;
+const CHUNKSIZE: u64 = 4096 * CHUNK_N_4K;
 
 struct Arguments {
     mbyte: u64,
     async_opt: bool,
     readwrite: bool,
-    number: u32,
+    wrnum: u32,
+    threadnums: u64,
+    keepfiles: bool,
 }
 
 fn argument_parser() -> Arguments {
@@ -27,7 +33,7 @@ fn argument_parser() -> Arguments {
                 .default_value("1024")
                 .value_parser(clap::value_parser!(u64))
                 .action(ArgAction::Set)
-                .help("Testfile size in MB")
+                .help("Sum of testfile size in MB")
                 .required(true),
         )
         .arg(
@@ -45,13 +51,29 @@ fn argument_parser() -> Arguments {
                 .help("Readwrite (default: write only)"),
         )
         .arg(
-            Arg::new("number")
+            Arg::new("wrnum")
                 .short('n')
                 .long("number")
                 .default_value("1000")
                 .value_parser(clap::value_parser!(u32))
                 .action(ArgAction::Set)
-                .help("Number of test (default: 1000)"),
+                .help("how many block will be write"),
+        )
+        .arg(
+            Arg::new("threadnums")
+                .short('t')
+                .long("threadnums")
+                .default_value("1")
+                .value_parser(clap::value_parser!(u64))
+                .action(ArgAction::Set)
+                .help("wrnum of threads"),
+        )
+        .arg(
+            Arg::new("keepfiles")
+                .short('k')
+                .long("keepfiles")
+                .action(ArgAction::SetTrue)
+                .help("keepfiles (default: no)"),
         )
         .get_matches();
 
@@ -59,11 +81,13 @@ fn argument_parser() -> Arguments {
         mbyte: *matches.get_one("size").unwrap(),
         async_opt: *matches.get_one("async").unwrap(),
         readwrite: *matches.get_one("readwrite").unwrap(),
-        number: *matches.get_one("number").unwrap(),
+        wrnum: *matches.get_one("wrnum").unwrap(),
+        threadnums: *matches.get_one("threadnums").unwrap(),
+        keepfiles: *matches.get_one("keepfiles").unwrap(),
     }
 }
 
-fn newfile(fname: &str, size_mb: u64) -> File {
+fn newfile(fname: &str, filesize: u64) -> File {
     let mut file = File::options()
         .create(true)
         .truncate(true)
@@ -71,8 +95,8 @@ fn newfile(fname: &str, size_mb: u64) -> File {
         .write(true)
         .open(fname)
         .unwrap();
-    let data = [0u8; 1024 * 1024];
-    for _ in 0..size_mb {
+    let data = [0u8; CHUNKSIZE as usize];
+    for _ in 0..filesize / CHUNKSIZE {
         file.write_all(&data).unwrap();
     }
     file.flush().unwrap();
@@ -80,29 +104,22 @@ fn newfile(fname: &str, size_mb: u64) -> File {
     file
 }
 
-fn speedtest(fvec: &mut MmapMut, number: u32, readwrite: bool, async_opt: bool) {
-    // write 4k in random place
-    let mut rng = rand::thread_rng();
-    let mut rndvec: Vec<usize> = vec![];
-    for _ in 0..number {
-        rndvec.push(rng.gen_range(0..fvec.len() / 4096));
-    }
-
-    let start = time::Instant::now();
+fn speedtest_testfunc(fvec: &mut MmapMut, rndvec: &[usize], readwrite: bool, async_opt: bool) {
+    let chunkusize = CHUNKSIZE as usize;
     if readwrite {
         // read and write
-        for rndnum in rndvec {
+        for &rndnum in rndvec {
             let fill = ((rndnum as u32) << 4) + 1;
-            for i in 0..1024 {
-                let stval = ((fvec[rndnum * 4096 + 4 * i] as u32) << 24)
-                    + ((fvec[rndnum * 4096 + 4 * i + 1] as u32) << 16)
-                    + ((fvec[rndnum * 4096 + 4 * i + 2] as u32) << 8)
-                    + (fvec[rndnum * 4096 + 4 * i + 3] as u32)
+            for i in 0..chunkusize / 4 {
+                let stval = ((fvec[rndnum * chunkusize + 4 * i] as u32) << 24)
+                    + ((fvec[rndnum * chunkusize + 4 * i + 1] as u32) << 16)
+                    + ((fvec[rndnum * chunkusize + 4 * i + 2] as u32) << 8)
+                    + (fvec[rndnum * chunkusize + 4 * i + 3] as u32)
                     + fill;
-                fvec[rndnum * 4096 + 4 * i] = (stval >> 24) as u8;
-                fvec[rndnum * 4096 + 4 * i + 1] = (stval >> 16) as u8;
-                fvec[rndnum * 4096 + 4 * i + 2] = (stval >> 8) as u8;
-                fvec[rndnum * 4096 + 4 * i + 3] = stval as u8;
+                fvec[rndnum * chunkusize + 4 * i] = (stval >> 24) as u8;
+                fvec[rndnum * chunkusize + 4 * i + 1] = (stval >> 16) as u8;
+                fvec[rndnum * chunkusize + 4 * i + 2] = (stval >> 8) as u8;
+                fvec[rndnum * chunkusize + 4 * i + 3] = stval as u8;
             }
             if !async_opt {
                 fvec.flush().unwrap();
@@ -110,13 +127,13 @@ fn speedtest(fvec: &mut MmapMut, number: u32, readwrite: bool, async_opt: bool) 
         }
     } else {
         // write only
-        for rndnum in rndvec {
+        for &rndnum in rndvec {
             let fill = ((rndnum as u32) << 4) + 1;
-            for i in 0..1024 {
-                fvec[rndnum * 4096 + 4 * i] = (fill >> 24) as u8;
-                fvec[rndnum * 4096 + 4 * i + 1] = (fill >> 16) as u8;
-                fvec[rndnum * 4096 + 4 * i + 2] = (fill >> 8) as u8;
-                fvec[rndnum * 4096 + 4 * i + 3] = fill as u8;
+            for i in 0..chunkusize / 4 {
+                fvec[rndnum * chunkusize + 4 * i] = (fill >> 24) as u8;
+                fvec[rndnum * chunkusize + 4 * i + 1] = (fill >> 16) as u8;
+                fvec[rndnum * chunkusize + 4 * i + 2] = (fill >> 8) as u8;
+                fvec[rndnum * chunkusize + 4 * i + 3] = fill as u8;
             }
             if !async_opt {
                 fvec.flush().unwrap();
@@ -124,43 +141,110 @@ fn speedtest(fvec: &mut MmapMut, number: u32, readwrite: bool, async_opt: bool) 
         }
     }
     fvec.flush().unwrap();
+}
+
+// --- MAIN ---
+
+fn main() {
+    let arg = argument_parser();
+    std::fs::create_dir_all(DIRNAME).unwrap();
+    let chunk_number = 1024 * 1024 * arg.mbyte / CHUNKSIZE;
+    let filesize = CHUNKSIZE * (chunk_number / arg.threadnums);
+
+    // create files for tests
+    let mut children = vec![];
+    for i in 0..arg.threadnums {
+        children.push(thread::spawn(move || -> f64 {
+            let mut mbps = 0.0;
+            let dirfilename = format!("{DIRNAME}/{FILENAME}-{i:06}");
+            if let Ok(file) = File::options().read(true).write(true).open(&dirfilename) {
+                if file.metadata().unwrap().len() != filesize {
+                    newfile(&dirfilename, filesize);
+                }
+            } else {
+                unsafe { libc::sync() };
+                let start = time::Instant::now();
+                newfile(&dirfilename, filesize);
+                let difftime = time::Instant::now() - start;
+                mbps = filesize as f64 / 1024. / 1024. / difftime.as_micros() as f64 * 1_000_000.;
+            };
+            mbps
+        }));
+    }
+    // join
+    let mut mbps = 0.0;
+    let mut mbps_ok = true;
+    for c in children {
+        let mbps_x = c.join().unwrap();
+        if mbps_x == 0.0 {
+            mbps_ok = false;
+        }
+        mbps += mbps_x;
+    }
+
+    // write 4k in random place
+    let mut rng = rand::thread_rng();
+    let mut rndvec: Vec<usize> = vec![];
+    // let chunkusize = CHUNKSIZE as usize;
+    for _ in 0..arg.wrnum / arg.threadnums as u32 {
+        rndvec.push(rng.gen_range(0..(chunk_number / arg.threadnums) as usize));
+    }
+
+    // Run test
+    let mut children = vec![];
+    let start = time::Instant::now();
+    for i in 0..arg.threadnums {
+        let rndvec = rndvec.clone();
+        let dirfilename = format!("{DIRNAME}/{FILENAME}-{i:06}");
+        children.push(thread::spawn(move || -> f64 {
+            let file = if let Ok(file) = File::options().read(true).write(true).open(&dirfilename) {
+                if file.metadata().unwrap().len() == filesize {
+                    file
+                } else {
+                    eprintln!("Error: filesize mismatch");
+                    std::process::exit(-1);
+                }
+            } else {
+                eprintln!("Error: no such file");
+                std::process::exit(-1);
+            };
+            // memmap
+            let mut fvec = unsafe { MmapOptions::new().map_mut(&file).unwrap() };
+            speedtest_testfunc(&mut fvec, &rndvec, arg.readwrite, arg.async_opt);
+            fvec.len() as f64
+        }));
+    }
+    // join
+    let sum_len: f64 = children.into_iter().map(|c| c.join().unwrap()).sum();
+
     let difftime = time::Instant::now() - start;
-    let msec_4k = difftime.as_micros() as f64 / 1000. / number as f64;
+    let msec_4k = difftime.as_micros() as f64 / 1000. / arg.wrnum as f64; // !!!
+
+    println!(
+        "\nFile length (sum): {:.2} GB, wrnum of random position 4kbyte test: {}",
+        sum_len / 1024. / 1024. / 1024.,
+        (arg.wrnum / arg.threadnums as u32) * arg.threadnums as u32
+    );
+    if mbps_ok {
+        println!("--> Linear write: {:.2} Mbyte/s  ({DIRNAME}/*)", mbps);
+    } else {
+        println!("--> Linear write: file already exists ({DIRNAME}/*)");
+    }
     println!(
         "--> {msec_4k:.3} msec/4k block write (iops: {:.1})",
         1000. / msec_4k
     );
-}
 
-fn main() {
-    let arg = argument_parser();
-    let mut mbps = 0.0;
-    let file = if let Ok(file) = File::options().read(true).write(true).open(FNAME) {
-        if file.metadata().unwrap().len() == 1024 * 1024 * arg.mbyte {
-            file
-        } else {
-            newfile(FNAME, arg.mbyte)
+    if !arg.keepfiles {
+        println!("Takarít -> {DIRNAME}");
+        for i in 0..arg.threadnums {
+            let dirfilename = format!("{DIRNAME}/{FILENAME}-{i:06}");
+            if fs::remove_file(&dirfilename).is_err() {
+                eprintln!("Remove error: {dirfilename} not exists!");
+            }
         }
-    } else {
-        unsafe { libc::sync() };
-        let start = time::Instant::now();
-        let file = newfile(FNAME, arg.mbyte);
-        let difftime = time::Instant::now() - start;
-        mbps = arg.mbyte as f64 / difftime.as_micros() as f64 * 1_000_000.;
-        file
-    };
-    // memmap
-    let mut fvec = unsafe { MmapOptions::new().map_mut(&file).unwrap() };
-    let print_len = fvec.len() as f64 / 1024. / 1024. / 1024.;
-    println!(
-        "\nFile length: {print_len:.2} GB, number of random position 4kbyte test: {}",
-        arg.number
-    );
-    if mbps > 0.0 {
-        println!("--> Linear write: {:.2} Mbyte/s  ({FNAME})", mbps);
-    } else {
-        println!("--> Linear write: file already exists ({FNAME})");
+        if std::fs::remove_dir(DIRNAME).is_err() {
+            eprintln!("--- Directory {DIRNAME} is not empty. Please remove manually! ---");
+        }
     }
-    // Run test
-    speedtest(&mut fvec, arg.number, arg.readwrite, arg.async_opt);
 }
